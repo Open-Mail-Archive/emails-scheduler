e81203cd02edefc6d4199245f41b4df76eecd45a
Mircea-Pavel Anton <mircea.anton99@yahoo.com>
2022-05-28 10:13:39 +0000

    Implement job enqueue-er.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('Hello world');
+import 'dotenv/config';
+import {JobData, RabbitMqHelper} from '@open-mail-archive/rabbitmq-helper';
+import {
+  GenericPayload,
+  DeletePayload,
+  InsertPayload,
+  RealtimeHelper,
+} from '@open-mail-archive/realtime-helper';
+import {EmailChannel, EmailPayload, EmailQueue} from '@open-mail-archive/types';
+import {RealtimeSubscription} from '@supabase/realtime-js';
+
+RealtimeHelper.client.connect();
+await RabbitMqHelper.init();
+
+const channel = RealtimeHelper.client.channel(
+  EmailChannel,
+) as RealtimeSubscription;
+
+channel.on('*', async (payload: GenericPayload) => {
+  let messagePayload: EmailPayload;
+
+  switch (payload.type) {
+    case 'INSERT':
+      messagePayload = (payload as InsertPayload<EmailPayload>).record;
+      break;
+    case 'DELETE':
+      messagePayload = (payload as DeletePayload<EmailPayload>).old_record;
+      break;
+    case 'UPDATE':
+      // nothing to do here
+      return;
+  }
+
+  await RabbitMqHelper.send(
+    EmailQueue,
+    new JobData<EmailPayload>(payload.type, messagePayload).toJson(),
+  );
+});
+
+channel.subscribe();
