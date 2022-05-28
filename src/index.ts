e21d32f4445dc0a46bac47749ad50281dc20d8c3
Mircea-Pavel Anton <mircea.anton99@yahoo.com>
2022-05-28 11:44:31 +0000

    Add logging.

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,36 @@ import {
 } from '@open-mail-archive/realtime-helper';
 import {EmailChannel, EmailPayload, EmailQueue} from '@open-mail-archive/types';
 import {RealtimeSubscription} from '@supabase/realtime-js';
+import {Logger} from '@open-mail-archive/logger';
 
+Logger.Instance.info({
+  trace: 'EmailsWorker',
+  message: 'Initializing helpers.',
+});
 RealtimeHelper.client.connect();
 await RabbitMqHelper.init();
+Logger.Instance.info({
+  trace: 'EmailsWorker',
+  message: 'Helpers initialized.',
+});
 
+Logger.Instance.info({
+  trace: 'EmailsWorker',
+  message: 'Creating the realtime subscription channel.',
+});
 const channel = RealtimeHelper.client.channel(
   EmailChannel,
 ) as RealtimeSubscription;
+Logger.Instance.debug({
+  trace: 'EmailsWorker',
+  message: 'Realtime channel created.',
+  data: channel,
+});
 
+Logger.Instance.info({
+  trace: 'EmailsWorker',
+  message: 'Attaching hooks to channel.',
+});
 channel.on('*', async (payload: GenericPayload) => {
   let messagePayload: EmailPayload;
 
@@ -36,5 +58,13 @@ channel.on('*', async (payload: GenericPayload) => {
     new JobData<EmailPayload>(payload.type, messagePayload).toJson(),
   );
 });
+Logger.Instance.info({
+  trace: 'EmailsWorker',
+  message: 'Hooks attached',
+});
 
+Logger.Instance.info({
+  trace: 'EmailsWorker',
+  message: 'Subscribing for events...',
+});
 channel.subscribe();
